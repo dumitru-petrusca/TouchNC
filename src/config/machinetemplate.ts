1e365827209fd20d9c1c44727f13c7c0523bb334
petrusca <petruscad@yahoo.com>
2025-09-01 17:16:08 -0700

    Remove the machine setting.

--- src/config/machinetemplate.ts
+++ src/config/machinetemplate.ts
@@ -110,7 +110,6 @@ export let machineTemplate = {
     name: string("None", 0, 32),
     board: string("None", 0, 32),
     meta: string("", 0, 32),
-    machine: select("MILL", ["MILL", "LATHE"])
   },
 
   various: {
